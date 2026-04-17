83229b350d378f6185b99397c29733ac2066de3d
DonovanAguilar <donovanaguilar441@gmail.com>
2026-04-16 18:32:47 -0600

    Commit 1

---
+++ src/test/java/com/EjercicioPractico2_DonovanAguilar/eventos/EventosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EjercicioPractico2_DonovanAguilar.eventos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
